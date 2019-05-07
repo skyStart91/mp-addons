9297dbacde2c4784538367ee2869d9dc8f984e1e
skyStart91 <1912959071@qq.com>
2019-05-07 21:45:23 +0800

    修改addons

--- src/Addons.php
+++ src/Addons.php
@@ -69,9 +69,9 @@ abstract class Addons
         $this->view = new View($config, Config::get('view_replace_str'));
 
         // 控制器初始化
-        if (method_exists($this, '_initialize')) {
-            $this->_initialize();
-        }
+        // if (method_exists($this, '_initialize')) {
+        //     $this->_initialize();
+        // }
     }
 
     /**
